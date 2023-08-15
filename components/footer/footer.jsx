146b7f055e49347b2b56bd00ebc600fc90a8b2ed
Jan Bryński <brynski.jan@gmail.com>
2023-08-15 11:14:25 +0200

    wip: footer

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -5,7 +5,22 @@ export default function Footer() {
     <footer>
       <div className={styles.wrapper}>
         <div className={styles.card}>
-          <img src="./vectors/footer.svg" alt="footer" />
+          <img
+            className={styles.logo}
+            src="./img/mcourt-logo-color.png"
+            alt="logo"
+          />
+          <img
+            className={styles.background}
+            src="./img/footer.png"
+            alt="footer"
+          />
+        </div>
+
+        <span>Copyright 2023 © MCOURT | All Rights Reserved</span>
+        <div className={styles.contact}>
+          <img src="./icons/phone.svg" alt="call-us" />
+          <p>Call us</p>
         </div>
       </div>
     </footer>
