8661e2357c97d07348288afe6344cb19c96c25d9
Jan Bryński <brynski.jan@gmail.com>
2023-08-16 15:05:56 +0200

    wip: projects

--- components/projects/projects.jsx
+++ components/projects/projects.jsx
@@ -1,48 +1,148 @@
-import styles from "./projects.module.scss";
 import Link from "next/link";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
+import { Navigation } from "swiper/modules";
+
+import styles from "./projects.module.scss";
 
 export default function Projects() {
   return (
-    <section className="overflow-hidden pb-[300px]">
-      <div className={styles.nav}>
+    <section className="pb-[300px] pl-[165px]">
+      <div className={styles.decor} />
+      <div className={styles.header}>
         <h2>Nasze projekty</h2>
-        <img src="./vectors/nav-arrows.svg" alt="navigation arrows" />
-      </div>
-      <div className={styles.cards}>
-        {/* <div className="w-[165px] h-[118px] bg-other-grey transform translate-y-[265px]" /> */}
-        <div className={styles.card}>
-          <img src="./img/project1.png" alt="project1" />
-          <h3>Project Number One</h3>
-          <Link href="/">
-            <span>View More</span>
-            <img src="./icons/little-arrow.svg" alt="arrow" />
-          </Link>
-        </div>
-        <div className={styles.card}>
-          <img src="./img/project2.png" alt="project2" />
-          <h3>Project Number One</h3>
-          <Link href="/">
-            <span>View More</span>
-            <img src="./icons/little-arrow.svg" alt="arrow" />
-          </Link>
-        </div>
-        <div className={styles.card}>
-          <img src="./img/project3.png" alt="project3" />
-          <h3>Project Number One</h3>
-          <Link href="/">
-            <span>View More</span>
-            <img src="./icons/little-arrow.svg" alt="arrow" />
-          </Link>
-        </div>
-        <div className={styles.card}>
-          <img src="./img/project4.png" alt="project4" />
-          <h3>Project Number One</h3>
-          <Link href="/">
-            <span>View More</span>
-            <img src="./icons/little-arrow.svg" alt="arrow" />
-          </Link>
+        <div className={styles.navigation}>
+          <div className="swiper-button-prev" />
+          <div className="swiper-button-next" />
+          <img src="./vectors/nav-arrows.svg" alt="navigation arrows" />
         </div>
       </div>
+      <Swiper
+        slidesPerView={3}
+        // spaceBetween={10}
+        modules={[Navigation]}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        }}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className={styles.card}>
+            <img
+              className={styles.thumbnail}
+              src="./img/project1.png"
+              alt="project1"
+            />
+            <h3>Project Number One</h3>
+            <Link href="/">
+              <span>View More</span>
+              <img src="./icons/little-arrow.svg" alt="arrow" />
+            </Link>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.card}>
+            <img
+              className={styles.thumbnail}
+              src="./img/project2.png"
+              alt="project2"
+            />
+            <h3>Project Number Two</h3>
+            <Link href="/">
+              <span>View More</span>
+              <img src="./icons/little-arrow.svg" alt="arrow" />
+            </Link>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.card}>
+            <img
+              className={styles.thumbnail}
+              src="./img/project3.png"
+              alt="project3"
+            />
+            <h3>Project Number Three</h3>
+            <Link href="/">
+              <span>View More</span>
+              <img src="./icons/little-arrow.svg" alt="arrow" />
+            </Link>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.card}>
+            <img
+              className={styles.thumbnail}
+              src="./img/project4.png"
+              alt="project4"
+            />
+            <h3>Project Number Four</h3>
+            <Link href="/">
+              <span>View More</span>
+              <img src="./icons/little-arrow.svg" alt="arrow" />
+            </Link>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.card}>
+            <img
+              className={styles.thumbnail}
+              src="./img/project1.png"
+              alt="project5"
+            />
+            <h3>Project Number Five</h3>
+            <Link href="/">
+              <span>View More</span>
+              <img src="./icons/little-arrow.svg" alt="arrow" />
+            </Link>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.card}>
+            <img
+              className={styles.thumbnail}
+              src="./img/project2.png"
+              alt="project6"
+            />
+            <h3>Project Number Six</h3>
+            <Link href="/">
+              <span>View More</span>
+              <img src="./icons/little-arrow.svg" alt="arrow" />
+            </Link>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.card}>
+            <img
+              className={styles.thumbnail}
+              src="./img/project3.png"
+              alt="project7"
+            />
+            <h3>Project Number Seven</h3>
+            <Link href="/">
+              <span>View More</span>
+              <img src="./icons/little-arrow.svg" alt="arrow" />
+            </Link>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.card}>
+            <img
+              className={styles.thumbnail}
+              src="./img/project4.png"
+              alt="project8"
+            />
+            <h3>Project Number Eight</h3>
+            <Link href="/">
+              <span>View More</span>
+              <img src="./icons/little-arrow.svg" alt="arrow" />
+            </Link>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 }
