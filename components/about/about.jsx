de0fcd55d744882d516a93c860928356bf446dfe
Jan Bryński <brynski.jan@gmail.com>
2023-08-17 16:49:15 +0200

    wip: rwd styles

--- components/about/about.jsx
+++ components/about/about.jsx
@@ -2,36 +2,34 @@ import styles from "./about.module.scss";
 
 export default function About() {
   return (
-    <section className="pt-[286px] pb-[299px]">
+    <section className={styles.container}>
       <div className={styles.background} />
 
-      <div className="flex justify-center">
-        <div className="flex">
-          <div className={styles.card}>
-            <h2>About us</h2>
-            <p>
-              Witamy w mCourt.eu, liderze w budowie i wyposażeniu kortów w
-              Polsce. Z dumą dostarczamy najwyższej jakości korty tenisowe, do
-              padla i pickleballa od ponad 10 lat. Nasze doświadczenie i
-              zrozumienie potrzeb klienta czynią nas niezrównanym wyborem dla
-              Twojego projektu.
-            </p>
-            <p>
-              Skontaktuj się z nami już dziś i przekonaj się, jak możemy pomóc w
-              realizacji Twojego wymarzonego kortu.
-            </p>
-            <div className={styles.bottom}>
-              <button className={styles.button}>Contact us</button>
-              <img className={styles.logo} src="./img/mcourt-logo-color.png" />
-            </div>
+      <div className={styles.cardsWrapper}>
+        <div className={styles.card}>
+          <h2>O nas</h2>
+          <p>
+            Witamy w mCourt.eu, liderze w budowie i wyposażeniu kortów w Polsce.
+            Z dumą dostarczamy najwyższej jakości korty tenisowe, do padla i
+            pickleballa od ponad 10 lat. Nasze doświadczenie i zrozumienie
+            potrzeb klienta czynią nas niezrównanym wyborem dla Twojego
+            projektu.
+          </p>
+          <p>
+            Skontaktuj się z nami już dziś i przekonaj się, jak możemy pomóc w
+            realizacji Twojego wymarzonego kortu.
+          </p>
+          <div className={styles.bottom}>
+            <button className={styles.button}>Contact us</button>
+            <img className={styles.logo} src="./img/mcourt-logo-color.png" />
           </div>
-
-          <img
-            className={styles.image}
-            src="./img/paddle-tennis.png"
-            alt="paddle"
-          />
         </div>
+
+        <img
+          className={styles.image}
+          src="./img/paddle-tennis.png"
+          alt="paddle"
+        />
       </div>
 
       <div className={styles.statsContainer}>
