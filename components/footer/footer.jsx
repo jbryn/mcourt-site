f4832ff8331b26c8deb9547b033d373f8dca4f9b
Jan Bryński <brynski.jan@gmail.com>
2023-08-21 16:57:53 +0200

    wip: footer

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -5,22 +5,28 @@ export default function Footer() {
     <footer>
       <div className={styles.wrapper}>
         <div className={styles.card}>
-          <img
-            className={styles.logo}
-            src="./img/mcourt-logo-color.png"
-            alt="logo"
-          />
-          <img
-            className={styles.background}
-            src="./img/footer.png"
-            alt="footer"
-          />
+          <img src="./img/mcourt-logo-color.png" alt="logo" />
+          <ul className={styles.menu}>
+            <li>Home</li>
+            <li>Courts</li>
+            <li>Tennis halls</li>
+            <li>Padel</li>
+            <li>Pickelball</li>
+          </ul>
+          <div className={styles.socials}>
+            <img src="./icons/facebook.svg" alt="facebook" />
+            <img src="./icons/instagram.svg" alt="instagram" />
+            <img src="./icons/twitter.svg" alt="twitter" />
+            <img src="./icons/linkedin.svg" alt="linkedin" />
+          </div>
         </div>
-
-        <span>Copyright 2023 © MCOURT | All Rights Reserved</span>
-        <div className={styles.contact}>
-          <img src="./icons/phone.svg" alt="call-us" />
-          <p>Call us</p>
+        <div className={styles.top} />
+        <div className={styles.bottom}>
+          <p>Copyright 2023 © MCOURT | All Rights Reserved</p>
+          <button className={styles.contact}>
+            <img src="./icons/phone.svg" alt="phone" />
+            <span>Call us</span>
+          </button>
         </div>
       </div>
     </footer>
