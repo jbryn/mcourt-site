40eb061f11a5660036aa4062ff9045103441fae3
Jan Bryński <brynski.jan@gmail.com>
2023-08-28 11:58:53 +0200

    chenges

--- components/courts/surfaces/surfaces.jsx
+++ components/courts/surfaces/surfaces.jsx
@@ -34,7 +34,7 @@ export default function Surfaces() {
             src="./img/surfaces/fast-track.png"
             width={540}
             height={406}
-            alt="antuka"
+            alt="fast-track"
           />
           <div className={styles.description}>
             <h2>Fast Track 15</h2>
@@ -59,7 +59,7 @@ export default function Surfaces() {
             src="./img/surfaces/padel.png"
             width={540}
             height={406}
-            alt="antuka"
+            alt="padel"
           />
           <div className={styles.description}>
             <h2>Padel courts</h2>
@@ -83,10 +83,35 @@ export default function Surfaces() {
             src="./img/surfaces/tennis-halls.png"
             width={540}
             height={406}
-            alt="antuka"
+            alt="tennis-halls"
           />
           <div className={styles.description}>
             <h2>Tennis halls</h2>
+            <h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur. Nibh suspendisse aliquam
+                sed libero eu faucibus commodo sagittis. Sit accumsan et mauris
+                fringilla elementum elit interdum scelerisque consectetur.
+              </p>
+              <p>
+                {" "}
+                Quis vitae pharetra aenean nibh pellentesque ac tristique metus
+                enim. Viverra mauris risus nunc nibh cras.
+              </p>{" "}
+              <p>Et tortor massa sit cras faucibus arcu commodo.</p>
+            </h3>
+          </div>
+        </div>
+        <div className={styles.surface}>
+          <img
+            className="order-1"
+            src="./img/surfaces/all-year.png"
+            width={540}
+            height={406}
+            alt="padel"
+          />
+          <div className={styles.description}>
+            <h2>All-year tennis halls</h2>
             <h3>
               <p>
                 Lorem ipsum dolor sit amet consectetur. Nibh suspendisse aliquam
