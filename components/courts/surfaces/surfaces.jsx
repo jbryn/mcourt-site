22f7bf3c8c69e0276aa6f5dee0b72f9cfc8e223f
Jan Bryński <brynski.jan@gmail.com>
2023-09-25 12:40:16 +0200

    feat: courts page

--- components/courts/surfaces/surfaces.jsx
+++ components/courts/surfaces/surfaces.jsx
@@ -6,8 +6,8 @@ export default function Surfaces() {
       <div className={styles.wrapper}>
         <div className={styles.surface}>
           <img
-            className="order-1"
-            src="./img/surfaces/antuka.png"
+            className="lg:order-1"
+            src="./img/courts/image1.jpeg"
             loading="lazy"
             width={540}
             height={406}
@@ -36,103 +36,87 @@ export default function Surfaces() {
         </div>
         <div className={styles.surface}>
           <img
-            src="./img/surfaces/fast-track.png"
+            src="./img/courts/image3.jpeg"
             loading="lazy"
             width={540}
             height={406}
             alt="fast-track"
           />
           <div className={styles.description}>
-            <h2>Fast Track 15</h2>
+            <h2>Korty ziemne / nawierzchnie z mączki ceglanej</h2>
             <h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur. Nibh suspendisse aliquam
-                sed libero eu faucibus commodo sagittis. Sit accumsan et mauris
-                fringilla elementum elit interdum scelerisque consectetur.
+                Nasze nawierzchnie z klasycznej mączki ceglanej czeskiego
+                producenta - <strong>Antuka Premium</strong> to połączenie
+                tradycji i nowoczesności. Wyróżniają się one doskonałą
+                przepuszczalnością wody, co oznacza, że kort po gwałtownych
+                opadach po kilkunastu minutach będzie zawsze gotowy do gry.
+                Dodatkowo, łatwa konserwacja sprawi, że Twój kort będzie zawsze
+                idealnie równy.
               </p>
               <p>
                 {" "}
-                Quis vitae pharetra aenean nibh pellentesque ac tristique metus
-                enim. Viverra mauris risus nunc nibh cras.
+                Odkryj nasze nawierzchnie z{" "}
+                <strong>mączki ceglanej Antuka</strong> i przekonaj się, jakie
+                korzyści może przynieść ten klasyczny wybór.
               </p>{" "}
-              <p>Et tortor massa sit cras faucibus arcu commodo.</p>
             </h3>
           </div>
         </div>
         <div className={styles.surface}>
           <img
-            className="order-1"
-            src="./img/surfaces/padel.png"
+            className="lg:order-1"
+            src="./img/courts/image4.jpeg"
             loading="lazy"
             width={540}
             height={406}
             alt="padel"
           />
           <div className={styles.description}>
-            <h2>Padel courts</h2>
+            <h2>Korty ze sztucznej trawy</h2>
             <h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur. Nibh suspendisse aliquam
-                sed libero eu faucibus commodo sagittis. Sit accumsan et mauris
-                fringilla elementum elit interdum scelerisque consectetur.
+                Marzysz o własnym <strong>korcie tenisowym</strong> przy domu?
+                Nasze <strong>korty tenisowe</strong> ze{" "}
+                <strong>sztucznej trawy</strong> są idealnym rozwiązaniem.
               </p>
               <p>
                 {" "}
-                Quis vitae pharetra aenean nibh pellentesque ac tristique metus
-                enim. Viverra mauris risus nunc nibh cras.
+                Stworzone z myślą o Twojej wygodzie, zapewniają doskonałą jakość
+                gry i niski poziom konserwacji. Przenieś swoją pasję tenisową na
+                prywatny kort i ciesz się nieograniczonymi grami o każdej porze
+                dnia i nocy. Odkryj naszą ofertę{" "}
+                <strong>kortów ze sztucznej trawy</strong> i rozpocznij swoją
+                tenisową przygodę już teraz
               </p>{" "}
-              <p>Et tortor massa sit cras faucibus arcu commodo.</p>
             </h3>
           </div>
         </div>
         <div className={styles.surface}>
           <img
-            src="./img/surfaces/tennis-halls.png"
+            src="./img/courts/image2.jpeg"
             loading="lazy"
             width={540}
             height={406}
             alt="tennis-halls"
           />
           <div className={styles.description}>
-            <h2>Tennis halls</h2>
+            <h2>Oświetlenie kortów tenisowych</h2>
             <h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur. Nibh suspendisse aliquam
-                sed libero eu faucibus commodo sagittis. Sit accumsan et mauris
-                fringilla elementum elit interdum scelerisque consectetur.
+                Nasze korty tenisowe wyróżniają się dzięki oświetleniu firmy
+                LedCourt, lidera w branży oświetleniowej.
               </p>
               <p>
                 {" "}
-                Quis vitae pharetra aenean nibh pellentesque ac tristique metus
-                enim. Viverra mauris risus nunc nibh cras.
+                Dzięki zaawansowanej technologii LED, zapewniamy nie tylko
+                doskonałą jakość oświetlenia, ale także oszczędność energii.
               </p>{" "}
-              <p>Et tortor massa sit cras faucibus arcu commodo.</p>
-            </h3>
-          </div>
-        </div>
-        <div className={styles.surface}>
-          <img
-            className="order-1"
-            src="./img/surfaces/all-year.png"
-            loading="lazy"
-            width={540}
-            height={406}
-            alt="padel"
-          />
-          <div className={styles.description}>
-            <h2>All-year tennis halls</h2>
-            <h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur. Nibh suspendisse aliquam
-                sed libero eu faucibus commodo sagittis. Sit accumsan et mauris
-                fringilla elementum elit interdum scelerisque consectetur.
+                Zainwestuj w renomowaną jakość, a Twój kort zawsze będzie
+                oświetlony w sposób idealny. Odkryj jasną przyszłość dla swojego
+                kortu tenisowego dzięki LedCourt
               </p>
-              <p>
-                {" "}
-                Quis vitae pharetra aenean nibh pellentesque ac tristique metus
-                enim. Viverra mauris risus nunc nibh cras.
-              </p>{" "}
-              <p>Et tortor massa sit cras faucibus arcu commodo.</p>
             </h3>
           </div>
         </div>
