a6deaba500788df33a7bf219e2237e8bf925b448
Jan Bryński <brynski.jan@gmail.com>
2023-09-26 12:50:47 +0200

    feat: grass page seo

--- pages/sztuczna-trawa.js
+++ pages/sztuczna-trawa.js
@@ -1,17 +1,41 @@
+import Head from "next/head";
 import Contact from "@/components/courts/contact/contact";
 import Grass from "@/components/courts/surfaces/grass/grass";
 import Hero from "@/components/shared/hero/hero";
 
 export default function CourtsPage() {
   return (
-    <main>
-      <Hero
-        backgroundUrl={"/img/grass/image5.jpeg"}
-        title="Sztuczna trawa"
-        subtitle={`Nawierzchnie z sztucznej trawy do kortów tenisowych to rozwiązania przyjazne środowisku i wydajne. Dzięki nam możesz cieszyć się nowoczesnością i minimalną konserwacją na swoim korcie. Dowiedz się więcej!`}
-      />
-      <Grass />
-      <Contact />
-    </main>
+    <>
+      <Head>
+        <title>
+          Korty Tenisowe z Nawierzchnią Sztucznej Trawy | Nowoczesność i
+          Minimalna Konserwacja
+        </title>
+        <meta
+          name="description"
+          content="Nawierzchnie z sztucznej trawy do kortów tenisowych to rozwiązania przyjazne środowisku i wydajne. Dzięki nam możesz cieszyć się nowoczesnością i minimalną konserwacją na swoim korcie. Dowiedz się więcej!"
+          key="desc"
+        />
+        <meta
+          property="og:title"
+          content="Korty Tenisowe z Nawierzchnią Sztucznej Trawy | Nowoczesność i
+          Minimalna Konserwacja"
+        />
+        <meta
+          property="og:description"
+          content="Nawierzchnie z sztucznej trawy do kortów tenisowych to rozwiązania przyjazne środowisku i wydajne. Dzięki nam możesz cieszyć się nowoczesnością i minimalną konserwacją na swoim korcie. Dowiedz się więcej!"
+        />
+        <meta property="og:image" content="./logos/mcourt-logo-color.png" />
+      </Head>
+      <main>
+        <Hero
+          backgroundUrl={"/img/grass/image5.jpeg"}
+          title="Sztuczna trawa"
+          subtitle={`Nawierzchnie z sztucznej trawy do kortów tenisowych to rozwiązania przyjazne środowisku i wydajne. Dzięki nam możesz cieszyć się nowoczesnością i minimalną konserwacją na swoim korcie. Dowiedz się więcej!`}
+        />
+        <Grass />
+        <Contact />
+      </main>
+    </>
   );
 }
