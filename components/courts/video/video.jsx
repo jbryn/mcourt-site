6ff724f942ec4b06789e7826d13fd641456cd857
Jan Bryński <brynski.jan@gmail.com>
2023-10-27 11:53:26 +0200

    feat: updated video component

--- components/courts/video/video.jsx
+++ components/courts/video/video.jsx
@@ -1,29 +1,13 @@
-import PlayVideo from "./fragments/play-video/play-video";
-import usePlayVideo from "./usePlayVideo";
 import styles from "./video.module.scss";
 
 export default function Video() {
-  const { active, toggle } = usePlayVideo();
-
   return (
     <section>
       <div className={styles.wrapper}>
         <h2>Zobacz film</h2>
-        <div className={styles.imageContainer}>
-          <img
-            src="https://media.graphassets.com/5wPJgkdSTuEYCp90yHkz"
-            width={583}
-            height={403}
-            alt="video"
-          />
-          <div className={styles.video}>
-            <PlayVideo
-              videoSrc="https://www.youtube.com/embed/Ra-rDKcs1Yg?si=WvgSPAq6PU71te_9"
-              active={active}
-              toggle={toggle}
-            />
-          </div>
-        </div>
+        <video controls autoPlay>
+          <source src="/video/video.mp4" type="video/mp4" />
+        </video>
       </div>
     </section>
   );
