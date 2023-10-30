3085cd11502f542a9ff5cad9eef084429da7a4d4
Jan Bryński <brynski.jan@gmail.com>
2023-10-30 10:30:50 +0100

    fix: video autoplay

--- components/courts/video/video.jsx
+++ components/courts/video/video.jsx
@@ -5,7 +5,7 @@ export default function Video() {
     <section>
       <div className={styles.wrapper}>
         <h2>Zobacz film</h2>
-        <video controls autoPlay>
+        <video controls>
           <source src="/video/video.mp4" type="video/mp4" />
         </video>
       </div>
