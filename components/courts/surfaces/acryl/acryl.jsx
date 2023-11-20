e230fec54864e086952a2f3bf9d84c3d0eaacc0c
Jan Bryński <brynski.jan@gmail.com>
2023-11-20 16:21:46 +0100

    wip: acryl page final

--- components/courts/surfaces/acryl/acryl.jsx
+++ components/courts/surfaces/acryl/acryl.jsx
@@ -15,7 +15,7 @@ export default function Acryl() {
             alt="antuka"
           />
           <div className={styles.description}>
-            <h2>Czym są nawierzchnie akrylowe?</h2>
+            <h2>Czym są akryle?</h2>
             <h3>
               <p>
                 <strong>Nawierzchnia akrylowa</strong> jest jednym z
@@ -35,78 +35,6 @@ export default function Acryl() {
                 <strong>Laykold</strong>, i przekształć swój kort tenisowy w
                 arenę mistrzów.
               </p>{" "}
-            </h3>
-          </div>
-        </div>
-        <div className={styles.surface}>
-          <Image
-            src="https://media.graphassets.com/J4Zd9OlZQoOePwqQYkUI"
-            loading="lazy"
-            width={540}
-            height={406}
-            alt="antuka"
-          />
-          <div className={styles.description}>
-            <h2>Akryl - przewagi</h2>
-            <h3>
-              <p>
-                <strong>Korty tenisowe twarde</strong> charakteryzują się
-                wyjątkową trwałością, elastycznością i odpornością na warunki
-                atmosferyczne. Nie straszna jest im deszczowa pogoda - ich
-                twardość oraz perfekcyjne wyrównanie pozwala na szybki odpływ
-                wody. Co więcej, oferują <strong>tenisistom</strong> doskonałą
-                przyczepność i charakteryzują się średniowysokim oraz szybkim
-                odbiciem piłki, co przekłada się na precyzję i kontrolę w
-                trakcie <strong>meczu tenisowego</strong>.
-              </p>
-            </h3>
-          </div>
-        </div>
-
-        <div className={styles.laykold}>
-          <div className="pt-[50px] lg:order-1">
-            <Image
-              src="https://media.graphassets.com/OGwwcsaCSTyjyOrFBmng"
-              loading="lazy"
-              width={600}
-              height={185}
-              alt="antuka"
-            />
-          </div>
-          <div className={styles.description}>
-            <h2>Tytuł sekcji</h2>
-            <h3>
-              <p>
-                W naszej ofercie znajdziesz nawierzchnię{" "}
-                <strong>Laykold</strong>. Tenisowa nawierzchnia akrylowa{" "}
-                <strong>Laykold</strong> to oficjalna nawierzchnia turniejowa na
-                kortach wielkoszlemowych US Open od 2020 roku. Od teraz i Ty
-                możesz mieć jeden z najlepszych kortów tenisowych na świecie.
-              </p>
-            </h3>
-          </div>
-        </div>
-
-        <div className={styles.surface}>
-          <Image
-            src="https://media.graphassets.com/4Fo65evrRQ2MZaTx8iQO"
-            loading="lazy"
-            width={540}
-            height={406}
-            alt="antuka"
-          />
-          <div className={styles.description}>
-            <h2>Tytuł sekcji</h2>
-            <h3>
-              <p>
-                Nawierzchnia <strong>Laykold</strong> to rozwiązanie, które
-                rewolucjonizuje korty tenisowe na całym świecie. Charakteryzuje
-                się wyjątkową jakością i trwałością, a także oferuje różne
-                systemy, które można dostosować do konkretnej preferencji
-                graczy. Warto bliżej przyjrzeć się dwóm głównym kategoriom
-                nawierzchni Laykold: systemom twardym i{" "}
-                <strong>amortyzowanym</strong>.
-              </p>
             </h3>
           </div>
         </div>
@@ -144,9 +72,89 @@ export default function Acryl() {
             </h3>
           </div>
         </div>
+
+        <div className={styles.laykold}>
+          <div className="lg:order-1">
+            <Image
+              src="https://media.graphassets.com/DJqtSYORGqo5eQN5dwqy"
+              loading="lazy"
+              width={540}
+              height={406}
+              alt="antuka"
+            />
+          </div>
+          <div className={styles.description}>
+            <h2>Zaufanie US Open</h2>
+            <h3>
+              <p>
+                Nawierzchnia <strong>Laykold</strong> to innowacyjne
+                rozwiązanie, które zdobyło uznanie w świecie tenisa. Jest to
+                syntetyczna nawierzchnia tenisowa, która oferuje wiele korzyści,
+                takie jak trwałość, odporność na warunki atmosferyczne oraz
+                doskonałe właściwości gry. Nawierzchnia Laykold stała się
+                oficjalną nawierzchnią turnieju US Open od 2020 roku, co
+                świadczy o wysokim standardzie tej nawierzchni.
+              </p>
+            </h3>
+          </div>
+        </div>
+
+        <div className={styles.surface}>
+          <Image
+            src="https://media.graphassets.com/4Fo65evrRQ2MZaTx8iQO"
+            loading="lazy"
+            width={540}
+            height={406}
+            alt="antuka"
+          />
+          <div className={styles.description}>
+            <h2>Nawierzchnia turniejowa</h2>
+            <h3>
+              <p>
+                Ponadto, nawierzchnia <strong>Laykold</strong> używana jest
+                także na innych znaczących turniejach, takich jak{" "}
+                <strong>
+                  Miami Open, New York Open, Winston Salem Open, San Diego Open
+                </strong>
+                , itp. co potwierdza jej popularność i zaufanie wśród
+                organizatorów ogromnych imprez tenisowych. Gracze często chwalą
+                tę nawierzchnię za jej równomierne odbicie piłki, elastyczność
+                oraz przyjemność z gry.
+              </p>
+            </h3>
+          </div>
+        </div>
+
+        <div className={styles.surface}>
+          <Image
+            src="https://media.graphassets.com/ahgdtH8S92z0H64tfAD8"
+            loading="lazy"
+            width={540}
+            height={406}
+            alt="antuka"
+          />
+          <div className={styles.description}>
+            <h2>Żywotność</h2>
+            <h3>
+              <p>
+                Czy ma sens coroczne zmienianie nawierzchni kortów tenisowych?
+                Tradycyjne rozwiązania sprawiają, że korty tracą swoją
+                amortyzację wraz z upływem lat, a ich właściwości stopniowo
+                spadają do zera w ciągu 3 lat od zainstalowania. Jednak dzięki
+                rewolucyjnej technologii Laykold Masters®, ta sytuacja ulega
+                zmianie! Nasze korty amortyzujące Laykold Masters® utrzymują
+                imponujące 95%-98% elastyczności nawet po 10 latach od
+                instalacji, co oznacza przełom w trwałości i wydajności kortów
+                tenisowych.
+              </p>
+            </h3>
+          </div>
+        </div>
+
         <div className="px-[30px] flex flex-col items-center gap-[50px] pt-[40px]">
-          <h2 className="font-semibold text-[42px] max-[600px]:text-[30px]">
-            Szybkość nawierzchni potwierdzają certyfikaty ITF:
+          <h2 className="font-semibold text-[42px] max-w-[1000px] text-center max-[600px]:text-[30px]">
+            Nawierzchnia certyfikowana przez Międzynarodową Federację Tenisa
+            (ITF):
           </h2>
           <Image
             className="rounded-[10px]"
@@ -156,6 +164,30 @@ export default function Acryl() {
             height={304}
             alt="laykold"
           />
+        </div>
+
+        <div className={styles.surface}>
+          <Image
+            src="https://media.graphassets.com/tmqrzSGFSuyhGKdMcE1y"
+            loading="lazy"
+            width={540}
+            height={406}
+            alt="antuka"
+          />
+          <div className={styles.description}>
+            <h2>Systemy dopasowane do potrzeb</h2>
+            <h3>
+              <p>
+                Nawierzchnia Laykold to rozwiązanie, które rewolucjonizuje korty
+                tenisowe na całym świecie. Charakteryzuje się wyjątkową jakością
+                i trwałością, a także oferuje różne systemy twarde i
+                amortyzowane, które można dostosować do konkretnej preferencji
+                graczy. Skontaktuj się z naszymi przedstawicielami w celu
+                dobrania odpowiedniego systemu nawierzchni Laykold do Twoich
+                potrzeb.
+              </p>
+            </h3>
+          </div>
         </div>
       </div>
     </section>
