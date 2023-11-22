7a10b8b67d8b3bf7b83fa1fea93815c241e73cfd
Jan Bryński <brynski.jan@gmail.com>
2023-11-22 13:05:12 +0100

    feat: padel page

--- components/padel/featues.jsx
+++ components/padel/featues.jsx
@@ -17,13 +17,14 @@ export default function Features() {
           <div className={styles.description}>
             <h2>Czym jest Padel?</h2>
             <h3>
-              Padel to dynamiczny i ekscytujący sport, który łączy w sobie
-              emocje tenisa z łatwością nauki dla wszystkich grup wiekowych.
-              Grając na specjalnie przystosowanych boiskach otoczonych szklanymi
-              ścianami, padel oferuje unikalne doznania, podnosząc rywalizację
-              na nowy poziom. Dzięki prostszym zasadom niż tradycyjny tenis,
-              padel jest dostępny dla każdego, a jednocześnie zapewnia wyzwania
-              dla doświadczonych graczy.
+              <strong>Padel</strong> to dynamiczny i ekscytujący sport, który
+              łączy w sobie emocje tenisa z łatwością nauki dla wszystkich grup
+              wiekowych. Grając na specjalnie przystosowanych boiskach
+              otoczonych szklanymi ścianami, <strong>padel oferuje</strong>{" "}
+              unikalne doznania, podnosząc rywalizację na nowy poziom. Dzięki
+              prostszym zasadom niż tradycyjny tenis,{" "}
+              <strong>padel jest dostępny dla każdego</strong>, a jednocześnie
+              zapewnia wyzwania dla doświadczonych graczy.
             </h3>
           </div>
         </div>
@@ -36,15 +37,135 @@ export default function Features() {
             alt="antuka"
           />
           <div className={styles.description}>
-            <h2>Czym jest Padel?</h2>
             <h3>
-              W sporcie tym można znaleźć kombinacje tenisa i squasha. Zasady
-              punktacji są identyczne jak w przypadku tenisa. Ze squasha’em
-              łączy go fakt, że sama gra odbywa się w zamkniętym korcie.
-              Przestrzeń do gry otacza specjalne ogrodzenie boiska do padla.
-              Może być ono wykonane z siatki drucianej. Bywają także korty
-              otoczone wysokimi ścianami z betonu lub szkła. Komfortową grę
-              zapewnia odpowiednia nawierzchnia boiska do padla.
+              W sporcie tym można znaleźć{" "}
+              <strong>kombinacje tenisa i squasha</strong>. Zasady punktacji są
+              identyczne jak w przypadku tenisa. Ze squasha’em łączy go fakt, że
+              sama gra odbywa się w zamkniętym korcie. Przestrzeń do gry otacza
+              specjalne <strong>ogrodzenie boiska do padla</strong>. Może być
+              ono wykonane z siatki drucianej. Bywają także korty otoczone
+              wysokimi ścianami z betonu lub szkła. Komfortową grę zapewnia{" "}
+              <strong>odpowiednia nawierzchnia boiska do padla</strong>.
+            </h3>
+          </div>
+        </div>
+        <div className={styles.surface}>
+          <Image
+            className="lg:order-1"
+            src="https://media.graphassets.com/Q9llusXyRhUmbEaHMe3Q"
+            loading="lazy"
+            width={540}
+            height={406}
+            alt="antuka"
+          />
+          <div className={styles.description}>
+            <h2>Specjalistyczna podbudowa</h2>
+            <h3>
+              Odpowiednio wykonana podbudowa gwarantuje{" "}
+              <strong>trwałość kortów na wiele lat</strong>. Przed wylaniem
+              płyty betonowej podłoże powinno zostać wyrównane przy użyciu
+              specjalistycznych maszyn profilujących (zdjęcie obok). Płyty
+              wykonujemy z betonu posadzkowego o odpowiedniej grubości lub z
+              kruszywa, oporowanego pasem betonu.
+            </h3>
+          </div>
+        </div>
+        <div className={styles.surface}>
+          <Image
+            src="https://media.graphassets.com/XQn1Q8zmSVKyv3gM2WvQ"
+            loading="lazy"
+            width={540}
+            height={406}
+            alt="antuka"
+          />
+          <div className={styles.description}>
+            <h2>Nawierzchnia do padla</h2>
+            <h3>
+              <p>
+                Ze względu na komfort gry oraz stosunkowo niskie nakłady pracy
+                podczas eksploatacji{" "}
+                <strong>
+                  najczęściej rekomendowaną nawierzchnią do gry w Padla jest
+                  sztuczna trawa
+                </strong>
+                . Jest to <strong>nawierzchnia syntetyczna</strong>, składająca
+                się z mocnego i elastycznego podkładu w który wtkane są włókna.
+              </p>
+              <p>
+                Najczęściej stosowana jest sztuczna trawa w kolorze niebieskim.
+              </p>
+            </h3>
+          </div>
+        </div>
+        <div className={styles.surface}>
+          <Image
+            className="lg:order-1"
+            src="https://media.graphassets.com/HtaMsG07S1qCwMxl7dnq"
+            loading="lazy"
+            width={540}
+            height={406}
+            alt="antuka"
+          />
+          <div className={styles.description}>
+            <h2>Oświetlenie</h2>
+            <h3>
+              Bardzo ważnym aspektem przy budowie kortu do padla jest
+              <strong>odpowiednie oświetlenie</strong>, które ma decydujący
+              wpływ na grę po zmroku. Piłka musi być bardzo dobrze widoczna jak
+              i zawodnicy znajdujący się na korcie. Rekomendowane jest
+              oświetlenie lampami LED dające minimum 200 luksów. Oświetlenie na
+              tym poziomie znajduje się w systemie standard, składa się ono z 4
+              lamp zamontowanych na czterech słupach o wysokości 6m wokół całego
+              kortu. Oświetlenie na poziomie 300 luksów montujemy w systemie
+              Panorama. Składa się ono z 8 lamp, które montowane są na czterech
+              słupach dookoła kortu.
+            </h3>
+          </div>
+        </div>
+        <h2 className="text-[42px] font-bold">SYSTEMY OGRODZEŃ</h2>
+        <div className={styles.surface}>
+          <Image
+            className="lg:order-1"
+            src="https://media.graphassets.com/ilJcndDRSWW9ab5yzt30"
+            loading="lazy"
+            width={540}
+            height={406}
+            alt="standard"
+          />
+          <div className={styles.description}>
+            <h2>System Standard</h2>
+            <h3>
+              <strong>Konstrukcja kortu</strong> w systemie standard wykonana
+              jest z stalowych ocynkowanych profili zamkniętych malowanych
+              proszkowo. Ściany boczne łączone są za pomocą śrub do słupów
+              ogrodzeniowych, zbudowane są z ram wypełnionych siatką malowaną
+              proszkowo.
+              <strong>Przeszklenia kortu</strong> na ścianach końcowych i
+              bocznych na odcinku czterometrowym od narożników składają się z
+              szkła hartowanego o grubości 10mm.
+            </h3>
+          </div>
+        </div>
+        <div className={styles.surface}>
+          <Image
+            className="lg:order-1"
+            src="https://media.graphassets.com/SdmikGKhSSK7EbQ1YUr1"
+            loading="lazy"
+            width={540}
+            height={406}
+            alt="panorama"
+          />
+          <div className={styles.description}>
+            <h2>System Panorama</h2>
+            <h3>
+              Konstrukcja kortu w systemie panorama bazuje na takich samych
+              elementach jak w przypadku systemu standard za wyjątkiem szkła
+              hartowanego, które w tym wypadku ma grubość 12mm. Nazwa panorama
+              wywodzi się z zwiększonej widoczności kortu, spowodowanej
+              usunięciem słupów w miejscach przeszkleń tj. na ścianach końcowych
+              i odcinkach czterometrowym na ścianach bocznych. W{" "}
+              <strong>systemie Panorama</strong> oświetlenie kortu jest na
+              poziomie 300 luksów.
             </h3>
           </div>
         </div>
--- pages/padel.js
+++ pages/padel.js
@@ -1,17 +1,40 @@
+import Head from "next/head";
 import Hero from "@/components/shared/hero/hero";
 import Contact from "@/components/home/contact/contact";
 import Features from "@/components/padel/featues";
 
 export default function CourtsPage() {
   return (
-    <main className="overflow-hidden">
-      <Hero
-        backgroundUrl="https://media.graphassets.com/tIO27usQHaiZ3ooTqjyy"
-        title="Padel"
-        subtitle="Specjalizujemy się w kompleksowej budowie boisk do padla. Serdecznie zachęcamy do zapoznania się z naszą ofertą oraz do nawiązania kontaktu. Nasz zespół z przyjemnością udzieli odpowiedzi na wszystkie pytania."
-      />
-      <Features />
-      <Contact />
-    </main>
+    <>
+      <Head>
+        <title>Budowa i montaż klatek i boiska do padla</title>
+        <meta
+          name="description"
+          content="Tworzymy perfekcyjne boiska do padla. Odkryj naszą ofertę i zainstaluj najwyższej jakości klatki do Padla, w ofercie różne systemy, błyskawiczna realizacja"
+          key="desc"
+        />
+        <meta
+          property="og:title"
+          content="Budowa i montaż klatek i boiska do padla"
+        />
+        <meta
+          property="og:description"
+          content="Tworzymy perfekcyjne boiska do padla. Odkryj naszą ofertę i zainstaluj najwyższej jakości klatki do Padla, w ofercie różne systemy, błyskawiczna realizacja"
+        />
+        <meta
+          property="og:image"
+          content="https://media.graphassets.com/GAIKjn52QECZ8IAZgHUO"
+        />
+      </Head>
+      <main className="overflow-hidden">
+        <Hero
+          backgroundUrl="https://media.graphassets.com/tIO27usQHaiZ3ooTqjyy"
+          title="Padel"
+          subtitle="Specjalizujemy się w kompleksowej budowie boisk do padla. Serdecznie zachęcamy do zapoznania się z naszą ofertą oraz do nawiązania kontaktu. Nasz zespół z przyjemnością udzieli odpowiedzi na wszystkie pytania."
+        />
+        <Features />
+        <Contact />
+      </main>
+    </>
   );
 }
